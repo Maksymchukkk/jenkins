daa92d5cb1ff37c026064537a2a2aeb44bac096b
Maksymchukkk <ivanmaksumchuk2021@gmail.com>
2023-12-06 16:40:51 +0200

    christmas

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello World
-          Merryy Cristmas
+          Merryy Cristmas!!!
         </h1>
       </header>
     </div>
